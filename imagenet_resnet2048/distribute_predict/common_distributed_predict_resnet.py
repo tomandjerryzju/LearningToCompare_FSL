29d8fa959fe845dc76eb197689741688effb0ff1
huangyanchun <huangyanchun@meituan.com>
2019-04-18 11:59:46 +0800

    m

--- imagenet_resnet2048/distribute_predict/common_distributed_predict_resnet.py
+++ imagenet_resnet2048/distribute_predict/common_distributed_predict_resnet.py
@@ -49,9 +49,6 @@ def prepare_pic_array(pic_path, img_height=224, img_width=224):
         img = img.resize((224, 224))
         img = img.convert('RGB')
         x = image.img_to_array(img)
-        print pic_path
-        print x
-        exit(1)
         if x.shape != (img_height, img_width, 3):
             print('filepath shape unmatched: %s shape=%s' % (pic_path, x.shape))
             return None
@@ -82,6 +79,9 @@ def prepare_feature_per_class(feature_encoder, image_root, support_num_per_class
                 try:
                     pic_arrays = np.array(pic_arrays)
                     pic_arrays = preprocess_input(pic_arrays)
+                    print filename
+                    print pic_arrays
+                    exit(1)
                     features = feature_encoder.predict_on_batch(pic_arrays)
                     feature = features.sum(axis=0)
                     break
