d32c041c85280547d23454555d51694bce28a29c
huangyanchun <huangyanchun@meituan.com>
2019-04-26 17:10:28 +0800

    m

--- imagenet_resnet2048/distribute_predict/common_distributed_predict_resnet.py
+++ imagenet_resnet2048/distribute_predict/common_distributed_predict_resnet.py
@@ -358,7 +358,7 @@ if __name__ == '__main__':
     print 'RelationNetwork_keras init done'
 
     # params need to be set
-    class_num = 6
+    class_num = 1
     support_num_per_class = 20
     support_set = "viewfs://hadoop-meituan/ghnn01/user/hadoop-dpsr/huangyanchun/fsl/support_set/jita_hdfs_merge"
     batch_predict(resnet_model, model, support_set, class_num, support_num_per_class, url_gen, output_path, batch_size=200)
