d374e0e60a082a6a44d4684ea4c7e628793c5f7e
huangyanchun <huangyanchun@meituan.com>
2019-04-26 14:10:11 +0800

    m

--- imagenet_resnet2048/dataset_split.py
+++ imagenet_resnet2048/dataset_split.py
@@ -3,8 +3,12 @@ import shutil
 import random
 
 folders = os.listdir("/opt/huangyanchun/workspace/datasets/imagenet/train_picvec")
-
-random.shuffle(folders)
-folders = folders[0:234]
+folders_filter = []
 for folder in folders:
+    if folder.startswith('n'):
+        folders_filter.append(folder)
+
+random.shuffle(folders_filter)
+folders_filter = folders_filter[0:234]
+for folder in folders_filter:
     shutil.move("/opt/huangyanchun/workspace/datasets/imagenet/train_picvec/%s" % folder, "/opt/huangyanchun/workspace/datasets/imagenet/val_picvec")
