27cf3fda9cc84ddfa2356f817f4c0a41fd9c945b
huangyanchun <huangyanchun@meituan.com>
2019-04-26 14:30:21 +0800

    m

--- imagenet_resnet2048/imagenet_resnet2048_train_few_shot_read_resnet2048_v2.py
+++ imagenet_resnet2048/imagenet_resnet2048_train_few_shot_read_resnet2048_v2.py
@@ -133,7 +133,7 @@ def main():
     relation_network_optim = torch.optim.Adam(relation_network.parameters(),lr=LEARNING_RATE)
     relation_network_scheduler = StepLR(relation_network_optim,step_size=20000,gamma=0.5)
 
-    checkpoint_path = str("./models/imagenet_resnet2048_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")
+    checkpoint_path = str("./models/imagenet_resnet2048_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot_imagenet.pkl")
     if os.path.exists(checkpoint_path):
         if USE_GPU:
             relation_network.load_state_dict(torch.load(checkpoint_path))
