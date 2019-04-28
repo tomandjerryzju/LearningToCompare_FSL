bf10ec52d6951e4b20faf781bbbce2124452230d
huangyanchun <huangyanchun@meituan.com>
2019-04-28 21:59:44 +0800

    m

--- imagenet_resnet2048/dataset_split.py
+++ imagenet_resnet2048/dataset_split.py
@@ -3,12 +3,12 @@ import shutil
 import random
 
 folders = os.listdir("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_train_picvec")
-folders_filter = []
-for folder in folders:
-    if folder.startswith('n'):
-        folders_filter.append(folder)
+# folders_filter = []
+# for folder in folders:
+#     if folder.startswith('n'):
+#         folders_filter.append(folder)
 
-random.shuffle(folders_filter)
-folders_filter = folders_filter[0:500]
-for folder in folders_filter:
-    shutil.move("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_train_picvec/%s" % folder, "/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_exclude_picvec")
+random.shuffle(folders)
+folders = folders[0:253]
+for folder in folders:
+    shutil.move("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_train_picvec/%s" % folder, "/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_val_picvec")
