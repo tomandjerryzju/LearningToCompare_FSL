0ca931570309f0a431f370c3a03f2d48655d4ab9
huangyanchun <huangyanchun@meituan.com>
2019-04-29 11:42:11 +0800

    m

--- imagenet_resnet2048/delete_little_cate.py
+++ imagenet_resnet2048/delete_little_cate.py
@@ -2,13 +2,15 @@ import os
 import shutil
 import random
 
-folders = os.listdir("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_val_picvec")
+path = "/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_val_picvec"
+folders = os.listdir(path)
 
 # random.shuffle(folders)
 # folders = folders[0:253]
 for folder in folders:
-    count = os.listdir("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_val_picvec/%s" % folder)
-    if count < 50:
-        shutil.rmtree("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_val_picvec/%s" % folder)
+    count = os.listdir(os.path.join(path, folder))
+    if len(count) < 50:
+        shutil.rmtree(os.path.join(path, folder))
+        print folder
 
     # shutil.move("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_train_picvec/%s" % folder, "/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_val_picvec")
