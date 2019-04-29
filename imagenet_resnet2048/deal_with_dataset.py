4c8e72580e5a85a2b5ae53b59e5163c59fcf9919
huangyanchun <huangyanchun@meituan.com>
2019-04-29 21:10:56 +0800

    m

---
+++ imagenet_resnet2048/deal_with_dataset.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import random
+
+folders = os.listdir("/opt/huangyanchun/workspace/datasets/imagenet/train_extent_picvec")
+# folders_filter = []
+for folder in folders:
+    if folder.startswith('n'):
+        shutil.rmtree("/opt/huangyanchun/workspace/datasets/imagenet/train_extent_picvec/%s" % folder)
+        print folder
+        # folders_filter.append(folder)
+
+# random.shuffle(folders)
+# folders = folders[0:253]
+# for folder in folders:
+#     shutil.move("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_train_picvec/%s" % folder, "/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_val_picvec")
