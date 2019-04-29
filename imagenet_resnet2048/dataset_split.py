b31b4899c127994a9145f8d712968d367f2bd4ac
huangyanchun <huangyanchun@meituan.com>
2019-04-29 21:20:04 +0800

    m

--- imagenet_resnet2048/dataset_split.py
+++ imagenet_resnet2048/dataset_split.py
@@ -2,13 +2,13 @@ import os
 import shutil
 import random
 
-folders = os.listdir("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_train_picvec")
+folders = os.listdir("/opt/huangyanchun/workspace/datasets/imagenet/train_extent_picvec")
 # folders_filter = []
 # for folder in folders:
 #     if folder.startswith('n'):
 #         folders_filter.append(folder)
 
 random.shuffle(folders)
-folders = folders[0:253]
+folders = folders[0:34]
 for folder in folders:
-    shutil.move("/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_train_picvec/%s" % folder, "/opt/huangyanchun/workspace/datasets/few_shot_learning/LearningToCompare_FSL/datas/imagenet_resnet2048/all_extentpic_ugcpic_checked/fsl_normal_dataset_val_picvec")
+    shutil.move("/opt/huangyanchun/workspace/datasets/imagenet/train_extent_picvec/%s" % folder, "/opt/huangyanchun/workspace/datasets/imagenet/train_extent_picvec/val_extent_picvec")
