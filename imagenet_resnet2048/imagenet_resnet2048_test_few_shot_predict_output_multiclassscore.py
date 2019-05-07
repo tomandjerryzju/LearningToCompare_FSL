ca450bb4307c83409f949ab23a413774aa4db1d0
huangyanchun <huangyanchun@meituan.com>
2019-05-07 21:46:27 +0800

    m

--- imagenet_resnet2048/imagenet_resnet2048_test_few_shot_predict_output_multiclassscore.py
+++ imagenet_resnet2048/imagenet_resnet2048_test_few_shot_predict_output_multiclassscore.py
@@ -18,12 +18,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
-import task_generator_test as tg
 import os
-import math
 import argparse
-import scipy as sp
-import scipy.stats
 from keras.applications.resnet50 import ResNet50
 from keras.applications.imagenet_utils import preprocess_input
 import tensorflow as tf
